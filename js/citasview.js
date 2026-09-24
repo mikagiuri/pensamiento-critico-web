8d2bdfa1ff8536080401e4b5395fc75b587a9c23
mikagiuri <mikagiuri@gmail.com>
2026-09-24 15:39:16 +0200

    Versión alumnado (2026-09-24)

--- js/citasview.js
+++ js/citasview.js
@@ -45,7 +45,7 @@
         '<figcaption class="cit-src"><span class="cit-a">' + esc(c.a) + '</span>' +
         (c.o ? ', <span class="cit-o">' + esc(c.o) + '</span>' : '') + '</figcaption></figure>';
     }).join("") + '</div>' +
-    '<p class="citcount">' + list.length + (list.length === 1 ? " cita" : " citas") + '.</p>';
+    '<p class="citcount">' + list.length + (list.length === 1 ? " frase" : " frases") + '.</p>';
   }
 
   function initCitas(){ if (!document.getElementById("citasbody")) return; renderCitFilter(); renderCitBody(); }
